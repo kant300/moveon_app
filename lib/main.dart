df1f10a29bbeed257e45a93e20428b7efd0a898f
IceP0tato <rlawodud0313@gmail.com>
2025-11-10 17:21:59 +0900

    251110_appBar, BottomNavigationBar 기본 레이아웃 작업

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,103 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => Main(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class Main extends StatefulWidget {
+  MainState createState() => MainState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MainState extends State<Main> {
+  int currentPage = 0;
+  dynamic pages = [
+    Center(child: Text("Menu")),
+    Center(child: Text("Location")),
+    Center(child: Text("Home")),
+    Center(child: Text("Community")),
+    Center(child: Text("MyPage")),
+  ];
+  
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          backgroundColor: Colors.white,
+          title: Column(
+            children: [
+              Text("mOveOn")
+            ]
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.notifications_on)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.login))
           ],
-        ),
+
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(1.0), // 높이 설정
+            child: Container(
+              color: Colors.grey, // 선 색상
+              height: 1.0,         // 선 두께
+            ),
+          ),
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: IndexedStack(index: currentPage, children: pages),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentPage,
+          onTap: (index) { setState(() {
+            currentPage = index;
+          });},
+
+          backgroundColor: Colors.white, // 바탕색을 밝게
+          selectedItemColor: Colors.blueAccent, // 선택된 아이템 색상
+          unselectedItemColor: Colors.grey.shade700, // 선택되지 않은 아이템 색상
+          selectedLabelStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+            color: Colors.blueAccent,
+          ),
+          unselectedLabelStyle: TextStyle(
+            fontSize: 12,
+            color: Colors.grey.shade700,
+          ),
+          type: BottomNavigationBarType.fixed,
+
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu),
+                label: "전체메뉴"
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.location_searching),
+                label: "내위치"
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "홈"
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.question_answer),
+                label: "커뮤니티"
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: "마이페이지"
+            )
+          ]
+      )
     );
   }
 }
