7abf6dfd0cd65b609b111cbf8727bc4e700156e8
kant300 <p.o.i.u.0987552@gmail.com>
2025-11-14 18:04:39 +0900

    온보딩-카테고리선택

--- lib/screens/onboarding/OnboardingAddress.dart
+++ lib/screens/onboarding/OnboardingAddress.dart
@@ -16,18 +16,43 @@ class OnboardingAddressState extends State<OnboardingAddress> {
       appBar: AppBar(
         title: const Text("주소 확인"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {// "다음" 버튼 클릭 시 다음 페이지로 이동
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => OnboardingCategory(), // 카테고리선택 페이지로 이동
-              ),
-            );
-          },
-          child: const Text("다음 단계"),
-        ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _colorBar(const Color(0xFF3DE0D2)),   // 진한 청록
+              const SizedBox(width: 24),
+              _colorBar(const Color(0xFF7FFFD4)),   // 연한 민트
+              const SizedBox(width: 24),
+              _colorBar(const Color(0xFFC5F6F6)),   // 더 연한 민트
+           ],
+          ),
+          Text("어디로 이사 오셨나요?"),
+          Text("새로운 동네 정보를 알려 드릴게요"),
+
+          ElevatedButton(
+            onPressed: () {// "다음" 버튼 클릭 시 다음 페이지로 이동
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => OnboardingCategory(), // 카테고리선택 페이지로 이동
+                ),
+              );
+            },
+            child: const Text("다음 단계"),
+          ),
+        ],
+      ),
+    );
+  }
+  Widget _colorBar(Color color) {
+    return Container(
+      width: 60,
+      height: 8,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(4),
       ),
     );
   }
--- lib/screens/onboarding/OnboardingComplete.dart
+++ lib/screens/onboarding/OnboardingComplete.dart
@@ -18,18 +18,45 @@ class OnboardingCompleteState extends State<OnboardingComplete>{
       appBar: AppBar(
         title: const Text("설정 완료"),
       ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () { // "다음" 버튼 클릭 시 다음 페이지로 이동
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Main(), // 메인 홈페이지로 이동
-                ),
-              );
-            },
-          child: const Text("시작하기"),
-        ),
+      body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _colorBar(const Color(0xFF3DE0D2)),   // 진한 청록
+                const SizedBox(width: 24),
+                _colorBar(const Color(0xFF3DE0D2)),   // 연한 민트
+                const SizedBox(width: 24),
+                _colorBar(const Color(0xFF3DE0D2)),   // 더 연한 민트
+              ],
+            ),
+
+            Text("이제 안전한 정착여정을 시작합니다."),
+
+
+
+            ElevatedButton(
+                onPressed: () { // "다음" 버튼 클릭 시 다음 페이지로 이동
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Main(), // 메인 홈페이지로 이동
+                    ),
+                  );
+                },
+              child: const Text("시작하기"),
+            ),
+          ]
+      ),
+    );
+  }
+  Widget _colorBar(Color color) {
+    return Container(
+      width: 60,
+      height: 8,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(4),
       ),
     );
   }
